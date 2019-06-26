bca8ed8a0f5989ea2c374629670ba7e60fbd31a1
demboo <demboo@tlen.pl>
2019-06-26 16:10:03 +0200

    init

---
+++ assets/js/main/ShoppingList/ShoppingList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <b-table striped :items="positions" :fields="{id:'id', name:'Nazwa', count:'Ilość', description:'Opis'}" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShoppingList",
+        data() {
+            return {
+                positions: [
+                    {
+                        id: 1,
+                        name: 'mleko',
+                        count: 1,
+                        description: 'w proszku'
+                    },
+                    {
+                        id: 1,
+                        name: 'chleb razowy',
+                        count: 6,
+                        description: 'w proszku'
+                    },
+                    {
+                        id: 1,
+                        name: 'jaja',
+                        count: 24,
+                        description: 'w proszku'
+                    },
+                    {
+                        id: 1,
+                        name: 'woda gazowana zgrzewka',
+                        count: 1,
+                        description: 'w proszku'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ assets/js/main/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import BootstrapVue from "bootstrap-vue";
+import ShoppingList from './ShoppingList/ShoppingList';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
+const app = {
+    render: h => h(ShoppingList),
+};
+
+new Vue(app).$mount('#main');
