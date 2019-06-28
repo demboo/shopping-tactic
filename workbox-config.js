8a86bb2337b049981e0ffd5f9ccf7ebc9a2a163a
demboo <demboo@tlen.pl>
2019-06-28 12:23:52 +0200

    shopping-tactic-hackathon

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{css,js,vue,json,lock,php,yaml,dist,twig,csv,meta,xml,log,md,neon,properties,ini,markdown,xsd,svg,rst,sh,bat,py,txt,html,sql,template,y,test,file-test,yml,ico,png,priv,pub,zip,crt,key,srl,default,exe,m4,w32,h,c,phpt,dot,phar,dat,copy,xlf,engine,xlsm,example,gif,inc,expected,rng,res,mo,po,ts,base64,data}"
+  ],
+  "swDest": "sw.js"
+};
